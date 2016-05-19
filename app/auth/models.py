b21626ac4dfb337107f0e487593bf1bdc3179cd1
liyocee <collinskivale@gmail.com>
2016-05-19 08:58:53 +0300

    add app

---
+++ app/auth/models.py
@@ -0,0 +1,43 @@
+from app import db
+
+
+class Base(db.Model):
+    """
+    # Define a base model for other database tables to inherit
+    """
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+
+class User(Base):
+    """
+    user model
+    """
+
+    __tablename__ = 'auth_user'
+
+    # User Name
+    name = db.Column(db.String(128), nullable=False)
+
+    # Identification Data: email & password
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(192), nullable=False)
+
+    # Authorisation Data: role & status
+    role = db.Column(db.SmallInteger, nullable=False)
+    status = db.Column(db.SmallInteger, nullable=False)
+
+    # New instance instantiation procedure
+    def __init__(self, name, email, password):
+
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
